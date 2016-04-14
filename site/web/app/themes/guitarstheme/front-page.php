b949cbe19b013cf4149ac8c2e7fbee92a8cce484
Nathan Gaw <nathan@nathangaw.com>
2016-04-14 15:57:00 +0100

    FEAT: Front page additions

--- site/web/app/themes/guitarstheme/front-page.php
+++ site/web/app/themes/guitarstheme/front-page.php
@@ -1,17 +1,19 @@
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
-
-<header>
-	<img src="app/themes/guitarstheme/assets/images/largenonretina.jpg">
-
+</div>
+</div>
+<div class="container-fluid top-section ">
+	<div class="col-md-12">
+		<img class="full-width-image" src="app/themes/guitarstheme/dist/images/guitar-header.jpg">
+	</div>
+</div>
+<hr>
+<h1 class="container headline brand-text centred-text">Gus Electric Guitars</h1>
+<h4 class="container brand-text centred-text">Guitars and basses that look, sound and feel different.<br>Hand-built in the UK by luthier Simon Farmer.</h4>
+<hr>
+<div class="quotes container-fluid" style="background-color: black; height: 150px; opacity: 0.7">
+	<p class="centred-text" style="color: white">AS FEATURED IN</p>
+</div>
 <br>
 <br>
-<h1 class="container col-md-12">Front page</h1>
-<div class="container">
-	<div class="col-md-3">
-		<p>Some test text to see how text works...</p>
-	</div>
-	<div class="col-md-6">
-		<p>And some more text...</p>
-	</div>
